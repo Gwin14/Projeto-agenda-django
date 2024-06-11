8001f34e579bb63aab9bb1fed26e6d237483e99f
Fábio Santos <fabiospinheira@gmail.com>
2024-06-11 14:37:31 -0300

    Login e logout

--- contact/views.py
+++ contact/views.py
@@ -3,7 +3,11 @@ from django.urls import reverse
 from django.db.models import Q
 from contact.models import Contact
 from contact.forms import ContactForm
+
 from contact.forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
 # Create your views here.
 
 
@@ -162,3 +166,31 @@ def register(request):
     }
 
     return render(request, 'contact/register.html', context=context)
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            return redirect('index')
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'contact/login.html',
+        context=context
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('login')
